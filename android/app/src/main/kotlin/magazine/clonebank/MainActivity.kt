725546c783aeb060471e45f33b34e6f224cb033f
PAULOFO <paulofo.forever@gmail.com>
2020-04-04 22:11:14 -0300

    App finalizado, apenas com um erro de layout no iOS

---
+++ android/app/src/main/kotlin/magazine/clonebank/MainActivity.kt
@@ -0,0 +1,6 @@
+package magazine.clonebank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+//import 'package:bancointer/app/splashscreen/splashscreen_page.dart';
+import 'package:clonebank/app/splashscreen/splashscreen_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Banco Inter Layout',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: SplashscreenPage(),
+    );
+  }
+}
